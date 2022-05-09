f241d381be93680d7df6dfd1752b9e8636311ce2
GeorgeTsif <gtsiff@gmail.com>
2022-05-09 12:47:50 +0300

    Add ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
